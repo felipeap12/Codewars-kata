69a07375be760b66f5cdf8a67f9ad2cf8af72f98
Felipe Pereira <felipealexandrepereira@gmail.com>
2026-05-19 09:53:35 -0300

    Add solution - CodeHub

---
+++ 8-kyu/convert-boolean-values-to-strings-yes-or-no/convert-boolean-values-to-strings-yes-or-no.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+​
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    if (word == true)
+      {
+      return "Yes";
+    }
+    else {
+      return "No";
+    }
+  }
+}
