c40665adb5425a1ee03503d171cb4d17c0731807
Felipe Pereira <felipealexandrepereira@gmail.com>
2026-05-19 09:49:33 -0300

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-summation/grasshopper-summation.cs
@@ -0,0 +1,15 @@
+using System;
+​
+public static class Kata 
+{
+    public static int summation(int num)
+    {
+       int total = 0 ;
+      for (int i= 0; i <= num; i++)
+        {
+        total += i ;
+      
+      }
+        return total;
+    }
+}
